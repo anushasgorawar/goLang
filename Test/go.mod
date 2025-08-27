b724707c15d51922068833f9201302c7a595f69b
anushasgorawar <anushasgorawar>
2025-08-27 10:22:56 -0400

    test

---
+++ Test/go.mod
@@ -0,0 +1,3 @@
+module github.com/anushasgorawar/goLang
+
+go 1.23.4
---
+++ Test/main.go
@@ -0,0 +1,12 @@
+package main
+
+func IsEven(n int) string{
+	if n%2==0{
+		return "Yes"
+	}
+	return "No"
+}
+
+func main(){
+
+}
